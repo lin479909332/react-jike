0187b3cf8a00164331031a48553fd8e24a469e74
lin479909332 <lin479909332@gmail.com>
2023-12-12 20:33:27 +0800

    feat(Login): 实现登录组件的表单校验

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -8,11 +8,32 @@ const Login = () => {
       <Card className="login-container">
         <img className="login-logo" src={logo} alt="" />
         {/* 登录表单 */}
-        <Form>
-          <Form.Item>
+        <Form validateTrigger="onBlur">
+          <Form.Item
+            name="mobile"
+            rules={[
+              {
+                required: true,
+                message: '请输入手机号码',
+              },
+              {
+                pattern:
+                  /^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\d{8}$/,
+                message: '手机号码格式不正确',
+              },
+            ]}
+          >
             <Input size="large" placeholder="请输入手机号" />
           </Form.Item>
-          <Form.Item>
+          <Form.Item
+            name="code"
+            rules={[
+              {
+                required: true,
+                message: '请输入验证码',
+              },
+            ]}
+          >
             <Input size="large" placeholder="请输入验证码" />
           </Form.Item>
           <Form.Item>
