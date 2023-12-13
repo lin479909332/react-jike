9d16d2ec72d306e9a7a095727ad9775ef49c2041
lin479909332 <lin479909332@gmail.com>
2023-12-13 21:52:09 +0800

    feat(modules/user.js): 新增请求登录的异步方法

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,8 @@
+import { request } from '@/utils'
 import { createSlice } from '@reduxjs/toolkit'
 
 const userStore = createSlice({
+  name: 'user',
   initialState: {
     token: '',
   },
@@ -13,8 +15,18 @@ const userStore = createSlice({
 
 const { setToken } = userStore.actions
 
+// 请求登录的异步方法
+const fetchLogin = (loginForm) => {
+  return async (dispatch) => {
+    const res = await request.post('/authorizations', loginForm)
+    if(res.message === 'OK'){
+      dispatch(setToken(res.data.token))
+    }
+  }
+}
+
 const userReducer = userStore.reducer
 
-export { setToken }
+export { setToken, fetchLogin }
 
 export default userReducer
