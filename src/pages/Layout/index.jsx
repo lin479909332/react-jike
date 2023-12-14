8c167e0e2c0b54c416636449e792ea8ec3b328ee
lin479909332 <lin479909332@gmail.com>
2023-12-14 20:16:38 +0800

    feat(Layout/index.jsx): 新增退出登录功能

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -4,7 +4,7 @@ import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import './index.scss'
-import { fetchUserInfo } from '@/store/modules/user'
+import { fetchUserInfo, clearUserInfo } from '@/store/modules/user'
 
 const { Header, Sider } = Layout
 
@@ -43,6 +43,11 @@ const GeekLayout = () => {
     dispatch(fetchUserInfo())
   }, [dispatch])
   const name = useSelector((state) => state.user.userInfo.name)
+  // 确认退出登录
+  const logOut = () => {
+    dispatch(clearUserInfo())
+    navigate('/login')
+  }
   return (
     <Layout>
       <Header className="header">
@@ -50,7 +55,7 @@ const GeekLayout = () => {
         <div className="user-info">
           <span className="user-name">{name}</span>
           <span className="user-logout">
-            <Popconfirm title="是否确认退出？" okText="退出" cancelText="取消">
+            <Popconfirm title="是否确认退出？" okText="退出" cancelText="取消" onConfirm={logOut}>
               <LogoutOutlined /> 退出
             </Popconfirm>
           </span>
