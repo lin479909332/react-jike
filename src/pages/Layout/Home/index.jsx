f9790adc7ca1867d3dfa9fbbbfb31e56dfbefc9b
lin479909332 <lin479909332@gmail.com>
2023-12-14 20:33:03 +0800

    feat(Home/index.jsx): 在Home组件渲染基础的图表

--- src/pages/Layout/Home/index.jsx
+++ src/pages/Layout/Home/index.jsx
@@ -1,7 +1,31 @@
+import * as echarts from 'echarts'
+import { useEffect, useRef } from 'react'
+
 const Home = () => {
+  const chartRef = useRef(null)
+  useEffect(() => {
+    const myChart = echarts.init(chartRef.current)
+    const option = {
+      xAxis: {
+        type: 'category',
+        data: ['Angular', 'React', 'Vue'],
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          data: [30, 70, 150],
+          type: 'bar',
+        },
+      ],
+    }
+
+    myChart.setOption(option)
+  }, [])
   return (
     <div>
-      Home
+      <div ref={chartRef} style={{ width: '400px', height: '300px' }}></div>
     </div>
   )
 }
