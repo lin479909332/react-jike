275b64c0a8056f7ea9e58b3fdce297ef64e1d2b5
lin479909332 <lin479909332@gmail.com>
2023-12-14 20:10:28 +0800

    feat(Layout/index.jsx): 在页面加载完成后动态渲染用户姓名

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,7 +1,10 @@
 import { Layout, Menu, Popconfirm } from 'antd'
 import { HomeOutlined, DiffOutlined, EditOutlined, LogoutOutlined } from '@ant-design/icons'
-import './index.scss'
 import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import './index.scss'
+import { fetchUserInfo } from '@/store/modules/user'
 
 const { Header, Sider } = Layout
 
@@ -34,12 +37,18 @@ const GeekLayout = () => {
   // 菜单高亮
   const location = useLocation()
   const selectedKey = location.pathname
+  // 渲染用户信息
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchUserInfo())
+  }, [dispatch])
+  const name = useSelector((state) => state.user.userInfo.name)
   return (
     <Layout>
       <Header className="header">
         <div className="logo" />
         <div className="user-info">
-          <span className="user-name">柴柴老师</span>
+          <span className="user-name">{name}</span>
           <span className="user-logout">
             <Popconfirm title="是否确认退出？" okText="退出" cancelText="取消">
               <LogoutOutlined /> 退出
