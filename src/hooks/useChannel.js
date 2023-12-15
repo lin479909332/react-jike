c779899f9129bb86483dc2a290255826f44c90bf
lin479909332 <lin479909332@gmail.com>
2023-12-15 23:33:38 +0800

    feat(hooks/useChannel.js): 新增自定义hook用于获取频道列表

---
+++ src/hooks/useChannel.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react'
+import { getChannelListAPI } from '@/apis/article'
+// 频道列表
+function useChannel() {
+  const [channelList, setChannelList] = useState([])
+  useEffect(() => {
+    getChannelList()
+  }, [])
+  // 获取频道列表的方法
+  const getChannelList = async () => {
+    const res = await getChannelListAPI()
+    if (res.message === 'OK') {
+      setChannelList(res.data.channels)
+    }
+  }
+  return {
+    channelList,
+  }
+}
+
+export { useChannel }
