c4e000da4cd5b055bbdf39fb6510b7f62335b0ee
lin479909332 <lin479909332@gmail.com>
2023-12-15 22:35:08 +0800

    feat(apis/article.js): 新增发布文章的api

--- src/apis/article.js
+++ src/apis/article.js
@@ -1,8 +1,18 @@
 import { request } from '@/utils'
 
+// 获取频道列表api
 export function getChannelListAPI() {
   return request({
     url: '/channels',
     method: 'GET',
+  })
+}
+
+// 发布文章api
+export function createArticleAPI(data) {
+  return request({
+    url: '/mp/articles?draft=false',
+    method: 'POST',
+    data,
   })
 }
